cfa2a3e0141618495d0e630d25696147d39688c6
thanhnguyen <skp2001vn@gmail.com>
2026-03-13 19:26:15 -0400

    LRUCacheTTL

---
+++ src/main/java/org/example/LRUCacheTTL/Main.java
@@ -0,0 +1,18 @@
+package org.example.LRUCacheTTL;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        LRUCacheTTL<Integer, String> cache = new LRUCacheTTL<>(2);
+
+        cache.put(1, "A", 2000);
+        cache.put(2, "B", 5000);
+
+        System.out.println(cache.get(1)); // A
+
+        Thread.sleep(3000);
+
+        System.out.println(cache.get(1)); // null (expired)
+    }
+}
