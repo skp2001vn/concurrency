afb386e547070f289fa9297db8f1dd7cd8e95e2e
thanhnguyen <skp2001vn@gmail.com>
2026-03-13 18:15:42 -0400

    AsyncJobQueue

---
+++ src/main/java/org/example/AsyncJobQueue/AsyncJobQueue.java
@@ -0,0 +1,76 @@
+package org.example.AsyncJobQueue;
+
+import java.util.concurrent.*;
+
+/**
+ * an async job queue with a fixed thread pool, retry logic, and a dead-letter queue for failed jobs.
+ */
+public class AsyncJobQueue {
+
+    private final BlockingQueue<Job> jobQueue =
+            new LinkedBlockingQueue<>();
+
+    private final BlockingQueue<Job> deadLetterQueue =
+            new LinkedBlockingQueue<>();
+
+    private final ExecutorService workers;
+
+    private final int maxRetries;
+
+    public AsyncJobQueue(int workerCount, int maxRetries) {
+
+        this.maxRetries = maxRetries;
+        workers = Executors.newFixedThreadPool(workerCount);
+
+        for (int i = 0; i < workerCount; i++) {
+            workers.submit(this::workerLoop);
+        }
+    }
+
+    public void submit(Job job) {
+        jobQueue.offer(job);
+    }
+
+    private void workerLoop() {
+
+        while (true) {
+            try {
+                Job job = jobQueue.take();
+                try {
+                    job.getTask().run();
+                    System.out.println(
+                            "Job success: " + job.getId()
+                    );
+                } catch (Exception e) {
+                    job.incrementRetry();
+                    if (job.getRetries() <= maxRetries) {
+                        System.out.println(
+                                "Retry job: " + job.getId()
+                        );
+
+                        jobQueue.offer(job);
+                    } else {
+                        System.out.println(
+                                "Job moved to DLQ: " + job.getId()
+                        );
+
+                        deadLetterQueue.offer(job);
+                    }
+                }
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public BlockingQueue<Job> getDeadLetterQueue() {
+        return deadLetterQueue;
+    }
+
+    public void shutdown() {
+        workers.shutdownNow();
+    }
+}
+
---
+++ src/main/java/org/example/AsyncJobQueue/Main.java
@@ -0,0 +1,25 @@
+package org.example.AsyncJobQueue;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        AsyncJobQueue queue =
+                new AsyncJobQueue(3, 2);
+
+        queue.submit(new Job("job1", () -> {
+            System.out.println("Running job1");
+        }));
+
+        queue.submit(new Job("job2", () -> {
+            throw new RuntimeException("failure");
+        }));
+
+        queue.submit(new Job("job3", () -> {
+            System.out.println("Running job3");
+        }));
+    }
+}
